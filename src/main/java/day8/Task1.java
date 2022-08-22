0ee210dc20fc967c45a5a40b9aea902c6aaeb30b
Elena Runeva <runeva.elena@gmail.com>
2022-08-22 14:56:14 +0300

    Day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,20 @@
 package day8;
-
 public class Task1 {
     public static void main(String[] args) {
+        String str = "0";
+        long startTime = System.currentTimeMillis();
+        for (int i=1; i<=20000; i++)
+            str = str + " "+i;
+        long stopTime = System.currentTimeMillis();
+       //System.out.println(str);
+        System.out.println("Program execution time for String, mc: " + (stopTime - startTime));
 
+        startTime = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder("0");
+        for (int i=1; i<=20000; i++)
+            sb.append(" ").append(i);
+        //System.out.println(sb);
+        stopTime = System.currentTimeMillis();
+        System.out.println("Program execution time for StringBuilder, mc: " + (stopTime - startTime));
     }
 }
