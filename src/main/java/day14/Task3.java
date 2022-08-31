1b20502d5673575f11d59273c85bc3ec647710b9
Elena Runeva <runeva.elena@gmail.com>
2022-08-31 10:49:59 +0300

    Day 14: исправление task3. Исправлено только задание 3, т.к. оно по сути реализует всю логику перехвата ошибок, что и первые 2 задания.

--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -10,33 +10,29 @@ public class Task3 {
     public static void main(String[] args) {
         File file = new File("D:\\people.txt");
         List<Person> people = parseFileToObjList(file);
-        if (people != null) //такое сравнение работает, хотя странно, ведь типы разные - ?
+        if (people != null)
             System.out.println(people);
     }
+
     public static List<Person> parseFileToObjList(File file) {
         List<Person> people = new ArrayList<>();
         Scanner sc = null;
         try {
             sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("The file is not found");
+            return null;
         }
-        catch (FileNotFoundException e) {
-            try {
-                throw new RuntimeException("The file is not found");
-            } catch (RuntimeException ex){
-                System.out.println(ex.getMessage());
-                return null;
-            }
-        }
-        while (sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             String person = sc.nextLine();
             String[] personData = person.split(" ");
-            if (personData.length != 2 || Integer.parseInt(personData[1])<=0) {
-                try {
+
+            try{
+                if (personData.length != 2 || Integer.parseInt(personData[1]) <= 0)
                     throw new RuntimeException("The file is incorrect");
-                } catch (RuntimeException e){
-                    System.out.println(e.getMessage());
-                    return null;
-                }
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                return null;
             }
             people.add(new Person(personData[0], Integer.parseInt(personData[1])));
         }
