716488c52235898d4c130359b80874b37c43b7ca
Elena Runeva <runeva.elena@gmail.com>
2022-09-03 20:16:50 +0300

    Day 19

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,61 @@
 package day19;
 
-/**
- * @author Neil Alishev
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class Task1 {
     public static void main(String[] args) {
+    //Чичиков - 601
 
+    File file = new File("D:\\dushi.txt");
+    countWords(file);
+    }
+    public static void countWords(File file){
+        Map<String, Integer> wordsMap = new HashMap<>();
+
+        try{
+            Scanner sc = new Scanner(file);
+            if (! sc.hasNextLine()){
+                throw new IOException();
+            }
+            sc.useDelimiter("[.,:;()?!\"\\s–]+");
+            //заполнить соответствие слово<->количество
+            while (sc.hasNext()){
+                String word = sc.next();
+                if(wordsMap.containsKey(word))
+                    wordsMap.put(word, wordsMap.get(word)+1);
+                else
+                    wordsMap.put(word, 1);
+            }
+            //взять значения, отсортировать в порядке убывания;
+            //сортировку вроде бы не проходили в видео, если надо было сделать другим способом, сообщите, каким
+            List<Integer> numbers = new ArrayList<>(wordsMap.values());
+            numbers.sort(Comparator.reverseOrder());
+
+            //записываю дополнительно в файл, т.к. в консоль снова выводит знаки вопроса
+            File file_out = new File("D:\\dushi_100words.txt");
+            PrintWriter pw = new PrintWriter(file_out);
+
+            //для каждой пары ключ-значение узнать, какую позицию в отсортированном массиве
+            //всех количеств слов занимает количество из этой пары (количество=значение):
+            //если позиция меньше 100 -> это одно из 100 наиболее частых слов.
+            //(вывод без сортировки, в задании не требовалось)
+            for(Map.Entry<String, Integer> word_number: wordsMap.entrySet()){
+                if(numbers.indexOf(word_number.getValue())<100) {
+                    System.out.printf("%-20s %d \n", word_number.getKey(), word_number.getValue());
+                    pw.println(" "+word_number.getKey() +"  "+ word_number.getValue());
+                }
+            }
+            pw.close();
+            sc.close();
+        } catch (
+        FileNotFoundException e) {
+            System.out.println("The file is not found");
+        } catch (IOException e) {
+            System.out.println("The file is empty");
+        }
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,48 @@
 package day19.task2;
 
-/**
- * @author Neil Alishev
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("D:\\taxi_cars.txt");
+        Map<Integer, Point> taxis = new HashMap<>();
 
+        try{
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                String[] taxiData = sc.nextLine().split(" "); //проверка корректности данных не проводится
+                taxis.put(Integer.parseInt(taxiData[0]), new Point(Integer.parseInt(taxiData[1]), Integer.parseInt(taxiData[2])));
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("The file is not found");
+        }
+
+        System.out.println("Insert 4 coordinates - (x1,y1) and (x2,y2)");
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int count = 0;
+
+        if(x1>=x2 || y1>=y2 || x1<-1 || x2<-1 || y1<-1 || y2<-1 || x1>100 || x2>100 || y1>100 || y2>100){
+            System.out.println("Coordinates are not correct");
+            return;
+        }
+        System.out.println("Car IDs in the square:");
+        for (Map.Entry<Integer, Point> taxi_entry : taxis.entrySet()){
+            Point taxi = taxi_entry.getValue();
+            if (taxi.getX()>x1 && taxi.getY()>y1 && taxi.getX()<x2 && taxi.getY()<y2) {
+                count++;
+                System.out.println(taxi_entry.getKey());
+            }
+        }
+        System.out.println("Total cars = "+count);
     }
 }
 
